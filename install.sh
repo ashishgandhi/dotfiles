23841d7b79fabcccc1c6d90091bc5b3a747ca894
Ashish Gandhi <ag@ashishgandhi.org>
2019-06-11 15:57:31 -0700

    install: split chsh and installs
    Install depend on some envrionment variables being set like GOBIN. So
    once we setup the shell we should start over again.

--- install.sh
+++ install.sh
@@ -1,25 +1,5 @@
 #!/bin/bash -e
 
-# Use prezto
-function link_file {
-    src="${PWD}/$1"
-    dst="${HOME}/${1/_/.}"
-
-    if [ -e "${dst}" ] && [ ! -L "${dst}" ]; then
-        mv $dst $dst.bak
-    fi
-
-    ln -sfh ${src} ${dst}
-}
-
-for i in _*
-do
-    link_file $i
-done
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-chsh -s /bin/zsh
-
 # Install packages
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install go python cmake ffmpeg ctags-exuberant fasd ripgrep jq clang-format youtube-dl fzf mas htop tig
