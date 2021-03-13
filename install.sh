a8f0eba3e519d542e689f5307602769051543fa1
Ashish Gandhi <ag@ashishgandhi.org>
2021-03-12 16:17:05 -0800

    config: ask to keep changes when closing documents

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ defaults write -g com.apple.trackpad.scaling -float 3
 defaults write -g InitialKeyRepeat -int 25
 defaults write -g KeyRepeat -int 2
 defaults write -g NSQuitAlwaysKeepsWindows -bool YES
+defaults write -g NSCloseAlwaysConfirmsChanges -bool YES
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -int 2
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool YES
 
