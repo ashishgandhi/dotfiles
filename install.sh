f970c114f1d037077adf97704096784b892d9b0c
Ashish Gandhi <ag@ashishgandhi.org>
2021-07-16 14:13:09 -0700

    config: enable Monterey tabs in Safari

--- install.sh
+++ install.sh
@@ -126,6 +126,7 @@ defaults write com.apple.Safari PrintHeadersAndFooters -bool NO
 defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool YES
 defaults write com.apple.Safari CanPromptForPushNotifications -bool NO
 defaults write com.apple.Safari NeverUseBackgroundColorInToolbar -bool YES
+defaults write com.apple.Safari ShowStandaloneTabBar -bool NO
 defaults write -g WebKitDeveloperExtras -bool YES
 
 # Contacts
