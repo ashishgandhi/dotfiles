9894fda5bb64a0d5821a19840536d32ab5bebd14
Ashish Gandhi <ag@ashishgandhi.org>
2024-08-14 18:40:39 -0700

    install: add Sink It

--- install.sh
+++ install.sh
@@ -53,8 +53,8 @@ brew install clang-format cmake ctags-exuberant ffmpeg fzf git go ipython jq mas
 brew install --cask appcleaner dash fantastical fork google-drive iina kaleidoscope maccy maestral signal suspicious-package
 
 # Install Mac App Store apps
-# 1Blocker, 1Password for Safari, BBEdit, Crouton, DaisyDisk, Hex Fiend, Ivory, Keka, Microsoft To Do, Monodraw, Paste, Reeder, TestFlight, Things, WhatsApp
-mas install 1365531024 1569813296 404009241 1461650987 411643860 1342896380 6444602274 470158793 1274495053 920404675 967805235 1529448980 899247664 904280696 1147396723
+# 1Blocker, 1Password for Safari, BBEdit, Crouton, DaisyDisk, Hex Fiend, Ivory, Keka, Microsoft To Do, Monodraw, Paste, Sink It, Reeder, TestFlight, Things, WhatsApp
+mas install 1365531024 1569813296 404009241 1461650987 411643860 1342896380 6444602274 470158793 1274495053 920404675 967805235 6449873635 1529448980 899247664 904280696 1147396723
 
 # Preferences
 
