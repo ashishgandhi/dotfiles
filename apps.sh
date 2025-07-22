a9be318c7e99039fb9879de036745ee09407a9a5
Ashish Gandhi <ag@ashishgandhi.org>
2025-07-21 22:45:26 -0700

    apps: add Wipr 2

--- apps.sh
+++ apps.sh
@@ -30,8 +30,8 @@ brew install clang-format cmake ctags-exuberant ffmpeg fzf git go ipython jq mas
 brew install --cask appcleaner dash dropbox fork google-drive iina kaleidoscope@3 mimestream signal suspicious-package
 
 # Install Mac App Store apps
-# 1Blocker, 1Password for Safari, BBEdit, Crouton, DaisyDisk, Hex Fiend, Ivory, Keka, Microsoft To Do, Monodraw, Paste, Sink It, Reeder, TestFlight, Things, Vinegar, WhatsApp
-mas install 1365531024 1569813296 404009241 1461650987 411643860 1342896380 6444602274 470158793 1274495053 920404675 967805235 6449873635 1529448980 899247664 904280696 1591303229 310633997
+# 1Blocker, 1Password for Safari, BBEdit, Crouton, DaisyDisk, Hex Fiend, Ivory, Keka, Microsoft To Do, Monodraw, Paste, Sink It, Reeder, TestFlight, Things, Vinegar, WhatsApp, Wipr 2
+mas install 1365531024 1569813296 404009241 1461650987 411643860 1342896380 6444602274 470158793 1274495053 920404675 967805235 6449873635 1529448980 899247664 904280696 1591303229 310633997 1662217862
 
 # Preferences
 
