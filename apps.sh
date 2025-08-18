18b35d28dbba14b141b44bb924a69038a6bbc938
Ashish Gandhi <ag@ashishgandhi.org>
2025-08-18 01:24:23 -0700

    apps: do not show badge for same day reminders

--- apps.sh
+++ apps.sh
@@ -176,11 +176,11 @@ defaults write com.apple.iCal 'Default duration in minutes for new event' -float
 defaults write com.apple.calculator ViewDefaultsKey -string Scientific
 defaults write com.apple.calculator PaperTapeVisibleDefaultsKey -bool YES
 
-# Reminder
+# Reminders
 # Specify time in hours format like 1000
 defaults write com.apple.remindd todayNotificationFireTime -int -1
 defaults write com.apple.remindd showRemindersAsOverdue -bool YES
-defaults write com.apple.remindd shouldIncludeRemindersDueTodayInBadgeCount -bool YES
+defaults write com.apple.remindd shouldIncludeRemindersDueTodayInBadgeCount -bool NO
 defaults write com.apple.remindd enableAssignmentNotifications -bool YES
 
 # Image Capture
