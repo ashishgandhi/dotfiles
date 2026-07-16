0dac1c603bb438d6edac58dd29dc1c90a893658c
Ashish Gandhi <ag@ashishgandhi.org>
2026-07-15 23:42:33 -0700

    apps: update name for ChatGPT app

--- apps.sh
+++ apps.sh
@@ -27,7 +27,7 @@ brew analytics off
 brew install fantastical ffmpeg fzf git go ipython jq mas ncdu python ripgrep tig vim yt-dlp zoxide
 
 # Install apps
-brew install --cask 1password appcleaner chatgpt dash@6 dropbox fork google-drive iina kaleidoscope@3 mimestream paste signal suspicious-package visual-studio-code
+brew install --cask 1password appcleaner chatgpt-classic dash@6 dropbox fork google-drive iina kaleidoscope@3 mimestream paste signal suspicious-package visual-studio-code
 
 # Install Mac App Store apps
 # 1Blocker, 1Password for Safari, Amphetamine, BBEdit, DaisyDisk, Flighty, Hex Fiend, Ivory, Keka, Monodraw, Reeder, Sink It, TestFlight, Things, uBlock Origin Lite, Vinegar, WhatsApp, Wipr 2
